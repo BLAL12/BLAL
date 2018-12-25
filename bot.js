9f82ff4a9d8bfc798959b8e5ea8a670471b6d5ec
BLAL12 <46130386+BLAL12@users.noreply.github.com>
2018-12-25 14:09:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("By al7midy");
-
+const ownerid = '518024839953645588';
 
 client.on("ready", () => {
 let channel =     client.channels.get("527067290508722179")
@@ -23,6 +23,46 @@ setInterval(function() {
 channel.send(`3`);
 }, 30)
 })
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "b") {
+        		        if (message.author.id != ownerid) return;
+    message.delete().catch(O_o=>{}); 
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
+a.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "b") {
+        		        if (message.author.id != ownerid) return;
+    message.delete().catch(O_o=>{}); 
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
+b.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "b") {
+        		        if (message.author.id != ownerid) return;
+    message.delete().catch(O_o=>{}); 
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
 a.login(process.env.BOT_TOKEN1);
